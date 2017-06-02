8c31819b3db810056642728f0dd18578d544cca4
Jordan Dodson <jordandodson22@gmail.com>
2017-06-02 11:55:20 -0500

    missed some files

--- client.py
+++ client.py
@@ -1,10 +1,17 @@
-import torrents
+import torrent
 import sys
 
 def main():
 
+  peers = []
+
+  for arg in sys.argv[1:]:
+    if arg.beginswith('-p'):
+      ip, raw_port = arg[2:].split(':')
+      peers.append((ip, int(raw_port)))
+
   if sys.argv[1] == 'add':
-    t = torrents.read_torrent(sys.argv[2])
+    t = torrent.read_torrent(sys.argv[2])
 
 if __name__ == '__main__':
   main()
--- torrent.py
+++ torrent.py
@@ -1,12 +1,88 @@
 import hashlib
 import datetime
 
-def read_torrent(file_name):
-  pass
 
-def create_torrent(file_name, comment):
+def parse_bencode(text, start=0):
 
-  piece_length = 2 ** 18
+  pos = start
+
+  if text[pos] == 'i':
+    pos += 1
+
+    while text[pos].isdigit():
+      pos += 1
+
+    if text[pos] != 'e':
+      raise Exception('Invalid bencoding of integer (no terminating e)')
+
+    # Skip the trailing 'e'
+    pos += 1
+
+    return int(text[start+1 : pos-1]), pos
+
+  elif text[pos].isdigit():
+    raw_len = text[pos]
+    pos += 1
+
+    while text[pos].isdigit():
+      raw_len += text[pos]
+      pos += 1
+
+    if text[pos] != ':':
+      raise Exception('Invalid bencoding of string (missing colon after length)')
+
+    pos += 1
+    str_len = int(raw_len) 
+    res = text[pos : pos + str_len]
+    pos += str_len
+    
+    return res, pos
+
+  elif text[pos] == 'l':
+    res = []
+    pos += 1
+
+    while text[pos] != 'e':
+      item, pos = parse_bencode(text, pos)
+      res.append(item)
+
+    return res, pos + 1
+
+  elif text[pos] == 'd':
+    res = dict()
+    pos += 1
+
+    while text[pos] != 'e':
+      key, pos = parse_bencode(text, pos)
+      value, pos = parse_bencode(text, pos)
+      res[key] = value
+
+    return res, pos + 1
+
+
+def infohash(torr_dict):
+  '''Compute the infohash of a torrent'''
+
+  benc = bytes(bencode(torr_dict['info']), 'ascii')
+
+  return hashlib.sha1(benc).hexdigest()
+
+
+def read_torrent_file(file_name):
+  '''Read a torrent file'''
+
+  with open(file_name) as f:
+    text = f.read()
+
+  return parse_bencode(text)[0]
+
+
+def create_torrent(file_name, piece_length=2**18, comment=''):
+  '''Generate torrent info for the given file.
+
+  Return a dictionary containing the torrent info for the given file.
+  '''
+
   hash_list = []
   file_length  = 0
 
@@ -16,7 +92,9 @@ def create_torrent(file_name, comment):
                 'name': file_name.split('/')[-1],
                 'piece length': piece_length
               },
-              'comment': comment
+              'comment': comment,
+              'created by': 'SimpleTorrent',
+              'encoding': 'ascii'
             }
 
   with open(file_name, 'rb') as f:
@@ -42,7 +120,10 @@ def create_torrent(file_name, comment):
  
 
 def bencode(data):
-  '''Bencode data''' 
+  '''Bencode data
+
+  Given a dictionary, return a bencoded bytestring of that dictionary.
+  ''' 
 
   if isinstance(data, int):
     return 'i{:d}e'.format(data)
@@ -57,8 +138,11 @@ def bencode(data):
 
 
 def create_torrent_file(input_file, output_file):
+  '''Create a torrent file for the given input file.'''
+
   t = create_torrent(input_file)
   fn = output_file if output_file.endswith('.torrent') else output_file + '.torrent'
+
   with open(fn, 'w') as f:
     f.write(bencode(t))
 
